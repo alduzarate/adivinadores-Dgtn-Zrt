427caf9b84b5065b4eb78e5e974ba491adab63bd
Aldana Zarate <31429603+alduzarate@users.noreply.github.com>
2017-10-08 13:03:22 -0300

    Update adivinadorTest.php

--- tests/adivinadorTest.php
+++ tests/adivinadorTest.php
@@ -5,7 +5,7 @@ use PHPUnit\Framework\TestCase;
 
 class adivinadorTest extends TestCase {
   public function testNro() {
-    $jugador1 = new Principiante;
+    $jugador1 = new Principiante();
     $this->assertGreaterThan($jugador1->sugerirNumeroSecreto(), 0) && $this->assertLessThan($jugador1->sugerirNumeroSecreto(), 1000);
 
   }
